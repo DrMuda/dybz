ca5f023f3d3b690bf3f887219221b0a56e9dd2ed
木大博士a <1487537353@qq.com>
2022-04-28 17:58:42 +0800

    修复： 修复在服务器上报错的问题

--- 01bzServerNodeJS/src/utils/getDHTML.js
+++ 01bzServerNodeJS/src/utils/getDHTML.js
@@ -1,32 +1,38 @@
 /*
  * @Author: LXX
  * @Date: 2022-04-27 10:33:22
- * @LastEditTime: 2022-04-28 15:46:56
+ * @LastEditTime: 2022-04-28 17:42:33
  * @LastEditors: LXX
  * @Description:
  * @FilePath: \dybz\01bzServerNodeJS\src\utils\getDHTML.js
  */
 
 const puppeteer = require("puppeteer");
+const Log = require("./Log");
 
 module.exports = async (url) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    //开启拦截器
-    await page.setRequestInterception(true);
-    await page.on("request", (interceptedRequest) => {
-        //判断加载的url是否以jpg或png结尾，符合条件将不再加载
-        if (interceptedRequest.url().endsWith(".jpg") || interceptedRequest.url().endsWith(".png")) {
-            interceptedRequest.abort();
-        } else {
-            interceptedRequest.continue();
-        }
-    });
-    await page.goto(url);
+    try {
+        const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
+        const page = await browser.newPage();
+        //开启拦截器
+        await page.setRequestInterception(true);
+        await page.on("request", (interceptedRequest) => {
+            //判断加载的url是否以jpg或png结尾，符合条件将不再加载
+            if (interceptedRequest.url().endsWith(".jpg") || interceptedRequest.url().endsWith(".png")) {
+                interceptedRequest.abort();
+            } else {
+                interceptedRequest.continue();
+            }
+        });
+        await page.goto(url);
 
-    await page.waitForTimeout(1000);
-    const content = await page.content();
-    page.close();
-    browser.close();
-    return content;
+        await page.waitForTimeout(1000);
+        const content = await page.content();
+        page.close();
+        browser.close();
+        return content;
+    } catch (error) {
+        Log.error(error);
+        return "<body><div class='neirong'>server error</div></body>";
+    }
 };
