9fc6be89f38e56259bd04707c9ccff1095a7b7c5
laixuexin <1487537353@qq.com>
2023-04-25 09:28:01 +0800

    feat: 移除python相关

--- start_all.bat
+++ start_all.bat
@@ -1,4 +1,3 @@
 @echo on 
 start start_server_nodejs.bat
-start start_server_python.bat
 start start_web.bat
--- start_server_python.bat
+++
@@ -1,6 +0,0 @@
-cd ServerPython
-python3 manage.py runserver 0.0.0.0:8011
-if errorlevel 1 goto Fail
-
-:Fail
-python manage.py runserver 0.0.0.0:8011
